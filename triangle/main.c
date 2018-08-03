6d66d480396ec64b9da1f4198b7619019d3bd92a
studentp_10 <IamSid25>
2018-08-03 15:11:19 +0530

    Initial Commit

---
+++ triangle/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  triangle
+//
+//  Created by Student P_10 on 03/08/18.
+//  Copyright © 2018 priya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+// insert code here...
+    printf("Hello, World!\n");
+    int b,h,a;
+    printf("enter the b and h");
+    scanf("%d%d",&b,&h);
+    a=0.5*b*h;
+    printf("area of triangle%d",a);
+    return 0;
+}
